8bf1aea38113ec7f4f07980f0b5f28915b8b9d71
rxmper <168703793+pulsecodes@users.noreply.github.com>
2025-12-30 01:17:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __dirname = process.cwd();
 const app = express();
 const server = http.createServer();
 
-app.set('trust proxy', 1);
+app.set('trust proxy', true);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -33,6 +33,10 @@ const bareServer = createBareServer("/bare/", {
 });
 const PORT = process.env.PORT || 8080
 
+function getIP(req) {
+  return req.headers['x-forwarded-for']?.split(',')[0] || req.headers['x-real-ip'] || req.socket.remoteAddress;
+}
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -41,6 +45,10 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/check-domain", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "static", "index.html"));
@@ -94,6 +102,9 @@ app.use((req, res) => {
 });
 
 server.on("request", (req, res) => {
+  const ip = getIP(req);
+  Object.defineProperty(req.socket, 'remoteAddress', { value: ip, writable: true, configurable: true });
+
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
@@ -102,12 +113,16 @@ server.on("request", (req, res) => {
 });
 
 server.on("upgrade", (req, socket, head) => {
+  const ip = getIP(req);
+  Object.defineProperty(socket, 'remoteAddress', { value: ip, writable: true, configurable: true });
+
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
 });
+
 
 server.on("listening", () => {
   console.log(`Snorlax's Cave listening on port ${PORT}`);
